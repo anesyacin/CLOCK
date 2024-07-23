376dc391638abcabcc900ac84332bc3b798b6500
anesyacin <129337013+anesyacin@users.noreply.github.com>
2024-07-23 17:01:15 +0100

    Update clock.js

--- clock.js
+++ clock.js
@@ -9,12 +9,20 @@ document.getElementById('hours').style.transform= `rotate(${hour}deg)`
 }
 Clock();
 setInterval(Clock,1000);
-const colorChangeElement = document.getElementById('colorChangeElement');
+let i = 0;
 
 function changeBackgroundColor() {
-    const letters = '0123456789ABCDEF';
-    colors=[#ffd,#ff3,#ddd,#aaa,#bbb,rgb(143, 132, 142),rgb(133, 143, 132),rgb(32, 92, 27),rgb(163, 77, 77),rgb(228, 137, 137),black,rgb(157, 137, 228),rgb(80, 43, 215),rgb(215, 43, 72),rgba(117, 147, 152, 0.607)]
-   
-    colorChangeElement.style.backgroundColor = colors[Math.floor(Math.random() * colors.length];
+    i = (i + 1) % 2;
+    const colors = ['#7a5a5a', 'rgba(27, 31, 32, 0.74)'];
+    
+    const colorChangeElement = document.getElementById('colorChangeElement');
+    colorChangeElement.style.backgroundColor = colors[i];
+    const clockElements = document.getElementsByClassName('clock');
+    for (let j = 0; j < clockElements.length; j++) {
+        clockElements[j].style.color = colors[(i + 1) % 2];
+        
+    }
+    document.getElementById('H1').style.color = colors[(i + 1) % 2];
 }
+
 setInterval(changeBackgroundColor, 1000);
